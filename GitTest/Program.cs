2315fb800e6cd852258ccd37e8c2b48cf8e085bd
Shiroime <spartan117di@hotmail.com>
2017-11-10 16:32:14 +0100

    Ajout de l'entier B

--- GitTest/Program.cs
+++ GitTest/Program.cs
@@ -14,6 +14,7 @@ namespace GitTest
         [STAThread]
         static void Main()
         {
+            int b;
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
